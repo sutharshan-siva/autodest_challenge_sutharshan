e09302e513e39f9710b4a2d9e141a6884d5474a7
Sutharshan Sivasankar <51187084+sutharshan-siva@users.noreply.github.com>
2022-08-06 00:34:07 +0800

    Add files via upload

---
+++ python_api_code.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import sys
+import logging
+log = logging.getLogger('werkzeug')
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        return jsonify({'message' : 'Hello, World!'})
+    else:
+        return "<p>Hello, World</p>"
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1: 
+        print("No arguments given")
+        log.setLevel(logging.ERROR)
+        app.run(host="0.0.0.0", port=8080)
+    else: 
+        debug_status = sys.argv[1] 
+        if debug_status == "debug":
+            app.run(host="0.0.0.0", port=8080, debug=True)
+        else:
+            print("passing arugment is incorrect and only debug option is supported")
+            exit()
